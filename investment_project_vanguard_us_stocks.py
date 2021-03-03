90b35f788d474b9ef3a4665465a2e843be9e3ba3
Michael Gadda <gaddami@oregonstate.edu>
2021-03-03 08:10:57 -0800

    Switched gears from data scraping vanguard and now D-S on regular equities

--- investment_project_vanguard_us_stocks.py
+++ investment_project_vanguard_us_stocks.py
@@ -2,10 +2,12 @@ import yfinance as yf
 import datetime as dt
 from selenium import webdriver
 from bs4 import BeautifulSoup
+
 import requests
 import pandas as pd 
 import numpy as np 
 import sklearn as sk 
+from sklearn.linear_model import LinearRegression
 import time 
 import json
 
@@ -15,6 +17,42 @@ stock_list = pd.read_csv(r"F:\investment_ds_project\all_us_stocks.csv")
 stock_list.set_index = stock_list['Symbol']
 stock_list = stock_list.drop(['Name', 'Last Sale','Net Change','% Change', 'Volume', 'IPO Year'], axis = 1)
 stock_list.shape
+
+#stock_list = stock_list.set_index(['Symbol'])
+stock_list.dropna(axis = 0, inplace = True)
+stock_list['% Change'].replace({'%': ''},inplace = True, regex = True)
+x = stock_list[['Market Cap']]
+y = stock_list[['% Change']]
+
+x1 = stock_list['Market Cap']
+y1 = stock_list['% Change']
+
+y.dtypes
+
+stock_actions = None
+historical_data = None
+dividend_list = [0]*10000
+split_list = [0]*10000
+for idx, symbol in enumerate(stock_list['Symbol']): 
+    if(idx < 1):
+        ticker = yf.Ticker(symbol)
+        stock_actions = ticker.actions
+        dividend_avg = stock_actions['Dividends'].mean()
+        splits_avg = stock_actions['Stock Splits'].mean()
+        dividend_list[idx] = dividend_avg
+        split_list[idx] = splits_avg
+        historical_data = ticker.history(period = 'MAX', interval = '1mo')
+        #print(historical_data)
+        open_price =  historical_data['Open']
+        close_price = historical_data['Close']
+        mlist = list(historical_data)
+        #print(historical_data.index[len(open_price)-12])
+        #print(open_price[len(open_price)-12])
+        one_year_change = (open_price[len(open_price)-12]-close_price[len(close_price)-1])/open_price[len(close_price)-1]
+        #print(one_year_change)
+
+
+
 
 
 #import mutual fund information from vanguard
@@ -39,6 +77,7 @@ inside_table5 = inside_table4.find('div', class_= 'tableContainer')
 inside_table6 = inside_table5.find('div', class_ = 'scrollingTables')
 inside_table7 = inside_table6.find('div', class_ = 'scrollingTableLeftSide ng-scope')
 inside_table8 = inside_table7.find('table', class_ = 'long-list dataTable scrollingTableLeft ng-isolate-scope')
-print(inside_table8)
+#print(inside_table8)
 rows = inside_table8.find_all('tbody', class_ = 'original ng-scope')[0]
-print(rows)
+#print(rows)
+
